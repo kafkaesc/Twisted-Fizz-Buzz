1a7f854a3d399b891c79d90bd878998c57b4bac3
kafkaesc <jared.a.hettinger@gmail.com>
2023-05-02 22:33:48 -0500

    Adding FizzBuzzApiResponse class.

---
+++ TwistedFizzBuzz/FizzBuzzApiResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace TwistedFizzBuzz
+{
+	public class FizzBuzzApiResponse
+	{
+		public int multiple;
+		public String word;
+
+		public FizzBuzzApiResponse(int multiple, String word)
+		{
+			this.multiple = multiple;
+			this.word = word;
+		}
+	}
+}
+
